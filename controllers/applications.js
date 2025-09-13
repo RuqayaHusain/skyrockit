d3728f169b60bdc6f245266280d9bdcda7224d7d
Ruqaya Husain <ruqayanader22@gmail.com>
2025-09-13 14:33:26 +0300

    edit application

--- controllers/applications.js
+++ controllers/applications.js
@@ -74,5 +74,45 @@ router.delete('/:applicationId', async (req, res) => {
     }
 });
 
+// Applications Edit Route
+router.get('/:applicationId/edit', async (req, res) => {
+    try {
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        // Find the application by the applicationId supplied from req.params
+        const application = currentUser.applications.id(req.params.applicationId);
+        // Render the show view, passing the application data in the context object
+        res.render('applications/edit.ejs', {
+        application: application,
+        });
+    } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// Applications Update Route
+router.put('/:applicationId', async (req, res) => {
+  try {
+    // Find the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the current application from the id supplied by req.params
+    const application = currentUser.applications.id(req.params.applicationId);
+    // Use the Mongoose .set() method
+    // this method updates the current application to reflect the new form
+    // data on `req.body`
+    application.set(req.body);
+    // Save the current user
+    await currentUser.save();
+    // Redirect back to the show view of the current application
+    res.redirect(
+      `/users/${currentUser._id}/applications/${req.params.applicationId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
 module.exports = router;
